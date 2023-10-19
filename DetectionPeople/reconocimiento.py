43e76e0ba029fd92368fb622f5197f6612e2410d
Alejandro Rojas <androv389@gmail.com>
2023-10-19 16:03:05 -0500

    Archivos de la libreria cv2

---
+++ DetectionPeople/reconocimiento.py
@@ -0,0 +1,31 @@
+import cv2
+
+cascade_classifier = cv2.CascadeClassifier('../data/haarcascade_fullbody.xml')
+
+cap = cv2.VideoCapture(0)
+
+if not cap.isOpened():
+    print("Error a la conexion")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("No se pudieron capturar cuadros.")
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    detections = cascade_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    for (x, y, w, h) in detections:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    cv2.imshow("Detección de Personas", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
